0218d317b8c2f42f52ed1ef4b40410bb5088c023
Tinker42 <aabcarius@mail.csuchico.edu>
2017-12-04 11:52:54 -0800

    Update StringStack.h

--- StringStack.h
+++ StringStack.h
@@ -1,1 +1,30 @@
+#ifndef STRINGSTACK_H
+#define STRINGSTACK_H
+#include "TrueStack.h"
+using namespace std;
 
+class node {
+  public:
+    node();
+    node(string, node*);
+    string getWord() {return word;}
+    void setWord(string newWord) {word = newWord;}
+    node* getNext() {return next;}
+    void setNext(node* newNext) {next = newNext;}
+  private:
+    string word;
+    node* next = nullptr;
+};
+
+class StringStack : public TrueStack {
+  private:
+    node* stackTop;
+  public:
+    StringStack() {stackTop = nullptr;}
+    string pop();
+    void push(string);
+    bool isEmpty();
+};
+
+
+#endif
